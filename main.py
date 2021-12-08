611ee1102e37016e03c97c8721408e7e9e3a429e
Xatrucho <marlonenriqueponcevillatoro@gmail.com>
2021-12-08 14:41:13 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import random 
+
+def guess(x): 
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        if guess < random_number:
+            print('sorry, guess again. too low.')
+        elif guess > random_number:
+            print('sorry, gess again, too high.')
+
+    print(f'yay, you got the number {random_number} correctly.')
+
+guess(10)
